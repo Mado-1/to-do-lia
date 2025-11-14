94c768b329518c6c9893e3ca4dd8ffde68412793
Mado-1 <mathusanpappa@hotmail.com>
2025-11-14 11:30:44 +0100

    Första commit

--- app/App.jsx
+++ app/App.jsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoadingScreen from './loadingScreen';
-import HomeScreen from './(dashborard)';
+import HomeScreen from './(dashboard)';
 
 const Stack = createNativeStackNavigator();
 
